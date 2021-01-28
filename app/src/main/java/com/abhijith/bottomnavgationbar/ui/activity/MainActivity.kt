ff562af6d24fa1c6c768262c6a28422ee8390d90
AbhijithMogaveera <abhialur8898@gmail.com>
2021-01-28 10:56:23 +0530

    init commit

--- app/src/main/java/com/abhijith/bottomnavgationbar/ui/activity/MainActivity.kt
+++ app/src/main/java/com/abhijith/bottomnavgationbar/ui/activity/MainActivity.kt
@@ -4,36 +4,39 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.abhijith.bottomnavgationbar.R
-import com.abhijith.bottomnavgationbar.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+
+    val bottomNavigationView: BottomNavigationView by lazy {
+        findViewById(R.id.bnv)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.apply {
-            setUpNavigation()
-            makeCreateIconLarge()
-        }
+        setContentView(R.layout.activity_main)
+        setUpNavigation()
+        makeCreateIconLarge()
+
     }
-    private fun ActivityMainBinding.setUpNavigation() {
+
+    private fun setUpNavigation() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragm) as NavHostFragment?
 
         NavigationUI.setupWithNavController(
-            bnv,
+            bottomNavigationView,
             navHostFragment!!.navController
         )
     }
 
-    private fun ActivityMainBinding.makeCreateIconLarge() {
-        val menuView = bnv.getChildAt(0) as BottomNavigationMenuView
+    private fun makeCreateIconLarge() {
+        val menuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
         val i = 2;
         val iconView = menuView.getChildAt(i).findViewById<View>(R.id.icon)
         val layoutParams = iconView.layoutParams
@@ -42,5 +45,4 @@ class MainActivity : AppCompatActivity() {
         layoutParams.width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50f, displayMetrics).toInt()
         iconView.layoutParams = layoutParams
     }
-
 }
